200d20ed4380affdb3696ead6876777d9e123ff9
Opubor Tony <opubortony@gmail.com>
2022-09-14 09:35:26 -0700

    ClipboardInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ClipboardInput from "./components/ClipboardInput";
+import Refer from "./components/Refer";
 import About from "./pages/About";
 import ForgotPassword from "./pages/ForgotPassword";
 import Home from "./pages/Home";
@@ -10,6 +12,8 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<ClipboardInput />} /> */}
+        {/* <Route path="/" element={<Refer />} /> */}
         <Route path="about" element={<About />} />
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
