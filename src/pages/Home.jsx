25cbb0823b379f9aa048ee653386b074998f8cb8
freemann firms <freemannfirm@gmail.com>
2022-09-14 17:13:24 +0100

    walletbalance component created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,11 @@
 import React from "react";
+import WalletBalance from "../components/walletbalance";
 import AppLayout from "../layouts/AppLayout";
 
 function Home() {
   return (
     <AppLayout>
-      <h1>Hellow</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
-        reiciendis?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
-        reiciendis?
-      </p>
+      <WalletBalance/>
     </AppLayout>
   );
 }
