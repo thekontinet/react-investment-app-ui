3e5a066090cd9a483315829377837fa226ed235b
freemann firms <freemannfirm@gmail.com>
2022-09-14 17:19:42 +0100

    fix merge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
-import WalletBalance from "../components/walletbalance";
-=======
-import Card from "../components/Card";
->>>>>>> 283be61f72b8369f72943da301176625b192f0c6
 import AppLayout from "../layouts/AppLayout";
 
 function Home() {
   return (
     <AppLayout>
-<<<<<<< HEAD
-      <WalletBalance/>
-=======
       <h1>Hellow</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
@@ -21,8 +13,6 @@ function Home() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
         reiciendis?
       </p>
-    <Card content="$2,000.05" title="Balance"/>
->>>>>>> 283be61f72b8369f72943da301176625b192f0c6
     </AppLayout>
   );
 }
