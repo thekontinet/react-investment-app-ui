67e3134af199004c65b09ba19b370fe7d4d88d8c
OGHENEMINE <66541923+OGHENEMINE@users.noreply.github.com>
2022-09-16 15:35:33 +0100

    resolved a conflict

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,9 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-<<<<<<< HEAD
 import Transfer from "./pages/Transfer";
 import Withdrawal from "./pages/Withdrawal";
-=======
 import Dashboard from "./pages/Dashboard";
->>>>>>> 2647fe5f8126d1b38fa0408aab926a27963436ce
 
 export default function App() {
   return (
@@ -21,13 +18,10 @@ export default function App() {
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
         <Route path="forgot" element={<ForgotPassword />} />
-<<<<<<< HEAD
         <Route path="withdrawal" element={<Withdrawal />} />
         <Route path="transfer" element={<Transfer />} />
-=======
         <Route path="deposit" element={<Deposit />} />
         <Route path="dashboard" element={<Dashboard />} />
->>>>>>> 2647fe5f8126d1b38fa0408aab926a27963436ce
       </Routes>
     </BrowserRouter>
   );
